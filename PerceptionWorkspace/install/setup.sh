eeb8f29a0f7a483bd412c45cfd9db05a6bf9317a
Keshav Mehndiratta <mehndirattakeshav@gmail.com>
2024-11-28 10:13:32 -0500

    Fixed issues with Perception Workspace

--- PerceptionWorkspace/install/setup.sh
+++ PerceptionWorkspace/install/setup.sh
@@ -7,7 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/skanderb/QADT2025/NavWorkspace/install
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/keshav/QADT2025/PerceptionWorkspace/install
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
@@ -32,10 +32,6 @@ _colcon_prefix_chain_sh_source_script() {
 # source chained prefixes
 # setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
 COLCON_CURRENT_PREFIX="/opt/ros/humble"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/skanderb/QADT2025/NavWorkspace/src/px4_ros_com/src/navTests/install"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
 
